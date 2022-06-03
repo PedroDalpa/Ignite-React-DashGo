0b47d4085ab5afbfc3639fd601834b9fc163f3ea
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-06-03 14:35:03 -0300

    separating components sidebar

---
+++ src/components/Sidebar/Index.tsx
@@ -0,0 +1,23 @@
+import { Box, Stack } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+
+export function Sidebar() {
+  return (
+    <Box as='aside' w='64' mr='8'>
+      <Stack spacing='12' align='flex-start'>
+        <NavSection title="Geral" >
+          <NavLink title="dashboard" icon={RiDashboardLine} />
+          <NavLink title="Usuários" icon={RiContactsLine} />
+        </NavSection>
+        <NavSection title="Automação" >
+          <NavLink title="Formulário" icon={RiInputMethodLine} />
+          <NavLink title="Automação" icon={RiGitMergeLine} />
+        </NavSection>
+
+      </Stack>
+    </Box>
+  )
+
+}
---
+++ src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,15 @@
+import { Icon, Link, LinkProps as ChakraLinkProps, Text } from "@chakra-ui/react";
+import { ElementType } from "react";
+
+interface NavLinkProps extends ChakraLinkProps {
+  icon: ElementType;
+  title: string;
+}
+export function NavLink({ title, icon, ...rest }: NavLinkProps) {
+  return (
+    <Link display='flex' alignItems='center' {...rest}>
+      <Icon as={icon} fontSize='20' />
+      <Text ml='4' fontWeight='medium' casing='capitalize'>{title}</Text>
+    </Link>
+  )
+}
