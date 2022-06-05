46c376f0d7a405f162ee963161965e112d3ba857
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-06-05 09:44:15 -0300

    make list users responsive

--- src/components/Pagination/Index.tsx
+++ src/components/Pagination/Index.tsx
@@ -1,13 +1,14 @@
-import { Box, HStack } from '@chakra-ui/react';
+import { Box, HStack, Stack } from '@chakra-ui/react';
 import { PaginationButton } from './PaginationButton';
 
 export function Pagination() {
   return (
-    <HStack
+    <Stack
       spacing='6'
       mt='8'
       justifyContent='space-between'
       align='center'
+      direction={['column', 'row']}
     >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
@@ -15,8 +16,11 @@ export function Pagination() {
       <HStack spacing='2'>
         <PaginationButton number={1} isCurrent={true} />
         <PaginationButton number={2} />
+        <PaginationButton number={3} />
+        <PaginationButton number={4} />
+        <PaginationButton number={5} />
       </HStack>
-    </HStack>
+    </Stack>
   )
 
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,14 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header/Index';
 import { Pagination } from '../../components/Pagination/Index';
 import { Sidebar } from '../../components/Sidebar/Index';
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  });
   return (
     <Box>
       <Header />
@@ -31,21 +35,23 @@ export default function UserList() {
           <Table colorScheme='whiteAlpha'>
             <Thead>
               <Tr>
-                <Th px='6' color='gray.300' width='8'>
+                <Th px={['4', '4', '6']} color='gray.300' width='8'>
                   <Checkbox colorScheme='pink' />
                 </Th>
                 <Th>
                   Usuários
                 </Th>
-                <Th>
-                  Data de cadastro
-                </Th>
+                {isWideVersion &&
+                  <Th>
+                    Data de cadastro
+                  </Th>
+                }
                 <Th width='8'></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px='6'>
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme='pink' />
                 </Td>
                 <Td>
@@ -54,21 +60,25 @@ export default function UserList() {
                     <Text fontSize='sm' color='gray.300'>pedro.dalpa@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  04 de Junho de 2022
-                </Td>
+                {isWideVersion &&
+                  <Td>
+                    04 de Junho de 2022
+                  </Td>
+                }
 
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='purple'
-                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion &&
+                  <Td>
+                    <Button
+                      as='a'
+                      size='sm'
+                      fontSize='sm'
+                      colorScheme='purple'
+                      leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                }
               </Tr>
             </Tbody>
           </Table>
