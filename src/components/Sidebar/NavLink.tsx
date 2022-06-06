46f649ec25b4c88162ab142a43cf117d2a5b6971
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-06-06 13:33:29 -0300

    alter color actived link in sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { Icon, Link as ChakraLink, LinkProps as ChakraLinkProps, Text } from '@chakra-ui/react';
-import Link from 'next/link';
 import { ElementType } from 'react';
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -9,11 +9,11 @@ interface NavLinkProps extends ChakraLinkProps {
 }
 export function NavLink({ title, icon, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href}>
-      <ChakraLink display='flex' alignItems='center' {...rest}>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display='flex' alignItems='center' {...rest} color='pink.600'>
         <Icon as={icon} fontSize='20' />
         <Text ml='4' fontWeight='medium' casing='capitalize'>{title}</Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   )
 }
