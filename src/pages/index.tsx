e04e7c54d52c21e1da3e2b1f72ff39df14492493
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-06-06 18:45:14 -0300

    use react-hook-form and yup in singIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
 import { Input } from '../components/Form/input';
+// controlled: faz um gerenciamento campo a campo
+// indicado para formulários menores. const [name, setName] = useState('')
+
+// UnControlled: faz um gerenciamento melhor do
+// formulário, melhor desempenho, mais indicado para formulário com diversos campos
+// é oque sera utilizado aq 
+
+
+interface SingInFormData {
+  email: string;
+  password: string;
+}
+
+const singInFormSchema = yup.object().shape({
+  email: yup.string().required('E-email obrigatório').email('Precisa ser um e-mail válido'),
+  password: yup.string().required('Senha obrigatória').min(8, 'Precisa ter 8 caracteres')
+})
 
 export default function SingIn() {
+
+  const {
+    register,
+    handleSubmit,
+    formState
+  } = useForm<SingInFormData>({ resolver: yupResolver(singInFormSchema) });
+
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SingInFormData> = async (values) => {
+    console.log(errors, values);
+    await new Promise(resolve => setTimeout(resolve, 2000))
+  }
+
   return (
     <Flex
       w='100vw'
@@ -17,29 +51,33 @@ export default function SingIn() {
         p='8'
         borderRadius={8}
         flexDirection='column'
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-
           <Input
             name='email'
             label='E-mail'
             type='email'
+            error={errors.email}
+            {...register('email')}
           />
 
           <Input
             name='password'
             label='Senha'
             type='password'
+            {...register('password')}
+            error={errors.password}
           />
-
-
         </Stack>
 
         <Button
           type='submit'
           marginTop={6}
           colorScheme='pink'
-          size='lg'>
+          size='lg'
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
 
