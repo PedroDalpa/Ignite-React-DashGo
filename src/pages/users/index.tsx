6464799df6b29bce443c6fb5dd03c6de02bb335f
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-06-06 22:48:47 -0300

    show dinamic users data table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,21 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -71,36 +85,41 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Pedro Dalpa</Text>
-                        <Text fontSize='sm' color='gray.300'>pedro.dalpa@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion &&
-                      <Td>
-                        04 de Junho de 2022
-                      </Td>
-                    }
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme='pink' />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight='bold'>{user.name}</Text>
+                            <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion &&
+                          <Td>
+                            {user.createdAt}
+                          </Td>
+                        }
 
-                    {isWideVersion &&
-                      <Td>
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                        >
-                          Editar
-                        </Button>
-                      </Td>
-                    }
-                  </Tr>
+                        {isWideVersion &&
+                          <Td>
+                            <Button
+                              as='a'
+                              size='sm'
+                              fontSize='sm'
+                              colorScheme='purple'
+                              leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        }
+                      </Tr>
+                    )
+                  })}
+
                 </Tbody>
               </Table>
               <Pagination />
