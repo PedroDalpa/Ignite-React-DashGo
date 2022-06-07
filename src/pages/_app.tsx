d293483244eff43c00aac64644dcc703c74b94d5
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-06-06 22:57:32 -0300

    show react-query devTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { SidebarDrawerProvider } from '../context/SideBarDrawerContext';
 import { makeServer } from '../service/mirage';
 import { theme } from '../styles/theme';
@@ -13,11 +14,14 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+
       <ChakraProvider theme={theme}>
         <SidebarDrawerProvider>
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
     </QueryClientProvider>
   )
 }
