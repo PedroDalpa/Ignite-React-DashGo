31b25fcaca12271c43a32b1e642d67fc6a8a9086
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-06-22 10:47:55 -0300

    make prefetch usar date on mouse hover

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,8 +4,7 @@ import {
   Checkbox,
   Flex,
   Heading,
-  Icon,
-  Spinner,
+  Icon, Link, Spinner,
   Table,
   Tbody,
   Td,
@@ -15,13 +14,15 @@ import {
   Tr,
   useBreakpointValue
 } from '@chakra-ui/react';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { useState } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header/Index';
 import { Pagination } from '../../components/Pagination/Index';
 import { Sidebar } from '../../components/Sidebar/Index';
+import { api } from '../../service/api';
 import { useUsers } from '../../service/hooks/useUsers';
+import { queryClient } from '../../service/queryClient';
 
 export default function UserList() {
   const [page, setPage] = useState(1)
@@ -38,6 +39,17 @@ export default function UserList() {
     lg: true
   });
 
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`);
+      console.log(response);
+
+      return response.data
+    }, {
+      staleTime: 1000 * 60 * 10
+    })
+  }
+
   return (
     <Box>
       <Header />
@@ -53,7 +65,7 @@ export default function UserList() {
                 <Spinner size='sm' color='gray.500' ml='4' />
               )}
             </Heading>
-            <Link href='/users/create' passHref>
+            <NextLink href='/users/create' passHref>
               <Button
                 as='a'
                 size='sm'
@@ -63,7 +75,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
 
           </Flex>
           {isLoading ? (
@@ -102,7 +114,9 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight='bold'>{user.name}</Text>
+                            <Link _hover={{ color: 'purple.400' }} onMouseEnter={() => handlePrefetchUser(Number(user.id))}>
+                              <Text fontWeight='bold'>{user.name}</Text>
+                            </Link>
                             <Text fontSize='sm' color='gray.300'>{user.email}</Text>
                           </Box>
                         </Td>
